c280983319668a5118ae4a0a7fe2e46efff45512
Sahil <107852714+sahilt65@users.noreply.github.com>
2023-02-12 15:00:49 +0530

    creating logic for signin and signup
    creating logic for signin and signup and made changes in UI

--- server/route/auth.js
+++ server/route/auth.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bcryptjs = require('bcryptjs')
 const User = require("../models/user")
 const authRouter = express.Router();
+const jwt = require('jsonwebtoken');
 
 
 authRouter.post("/api/signup", async function (req, res){
@@ -13,7 +14,7 @@ authRouter.post("/api/signup", async function (req, res){
         const existingUser = await User.findOne({ email });
 
         if(existingUser){
-            return res.status(400).json({msg : 'User with sam email already exists!'}); 
+            return res.status(400).json({msg : 'User with same email already exists!'}); 
         }
 
         const hashedPassword = await bcryptjs.hash(password, 8);
@@ -42,6 +43,33 @@ authRouter.post("/api/signup", async function (req, res){
 })
 
  //Sign In Route
- 
+authRouter.post('/api/signin', async (req, res)=>{
+
+    try{
+        const{email, password} = req.body;
+
+        const user = await User.findOne({email});
+
+
+        if(!user){
+            return res.status(400).json({msg : 'User does not exist please create account to sign in'}); 
+        }
+
+        const isMatch = await bcryptjs.compare(password, user.password);
+
+        if(!isMatch){
+            return res.status(400).json({msg : 'Incorrect Password'}); 
+        }
+
+        const token = jwt.sign({id : user._id}, "passwordKey");
+        res.json({token, ...user._doc })
+
+    }catch(e){
+        res.status(500).json({error : e.message});
+        
+    }
+
+})
+
 
 module.exports = authRouter;
